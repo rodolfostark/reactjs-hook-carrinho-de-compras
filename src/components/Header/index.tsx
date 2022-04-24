63d2301a3938b6eb979f0e4f5057f9665bd2c257
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2022-04-24 14:41:49 -0300

    finishing all functional features to fix the tests

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,20 +6,20 @@ import { Container, Cart } from './styles';
 import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
-  // const { cart } = useCart();
-  // const cartSize = // TODO;
+  const { cart } = useCart();
+  // count unique items in cart
+  const cartSize = cart.length;
 
   return (
     <Container>
       <Link to="/">
         <img src={logo} alt="Rocketshoes" />
       </Link>
-
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
           <span data-testid="cart-size">
-            {/* {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`} */}
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
           </span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
